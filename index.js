5b8a4379d2e7d585f4daaef04c84a091da171a89
Sumitsingh39499 <sumitsingh22012005@gmail.com>
2026-02-27 23:30:13 +0530

    Remove node_modules and add gitignore

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
-
+import pg from "pg";
+//For storeing the search history in db
+const  db = new pg.Client({
+    user:"postgres",
+    host:"localhost",
+    database:"Weather",
+    password:"Sumits",
+    port:5432
+});
+db.connect();
 const app = express();
 const port = 3000;
 const apiURL = `https://api.openweathermap.org/data/2.5`;
@@ -14,78 +23,129 @@ app.get("/",(req,res)=>{
     res.render("index.ejs",{content:"",type:null});
 });
 
-app.get("/guide",(req,res)=>{
-    res.render("guide.ejs");
-});
-
-app.get("/city",(req,res)=>{
-    res.render("city.ejs");
-});
-
-app.get("/units",(req,res)=>{
-    res.render("units.ejs")
-});
-
 app.get("/weather",async (req,res)=>{
-    const city = req.query.city;
+    const startTime = Date.now();
+
+    const query = req.query.city;
     const units = req.query.units
-    if (!city) return res.status(400).send("City is required!");
+    const ip=req.ip;
+    const agent=req.headers["user-agent"];
+
+    if (!query) return res.status(400).send("City is required!");
     try{
     const result = await axios.get(apiURL+"/weather",{
         params:{
-            q:city,
+            q:query,
             units: units,
             appid : yourapikey
         }
     });
+
+    const data=result.data;
+    const duration=Date.now()-startTime;
+
+    //extract API values 
+    const city = data.name;
+    const country=data.sys.country;
+    const lat=data.coord.lat;
+    const lon=data.coord.lon;
+
+    //Insert into DB
+    await db.query("INSERT INTO search_history(query,city,country,latitude,longitude,units, ip_address,user_agent,response_time_ms,result_status)VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)",[query,city,country,lat,lon,units,ip,agent,duration,"success"]);
     res.render("index.ejs",{content :result.data ,type:"weather"});
 }catch(error){
+    const duration = Date.now()-startTime;
+    //log failed
+    await db.query("INSERT INTO search_history(query,units,ip_address,user_agent,response_time_ms,result_status) VALUES ($1,$2,$3,$4,$5,$6)",[query,units,ip,agent,duration,"failed"]);
+
     res.status(404).send(error.message);
 }
 });
 
 app.get("/forecast",async(req,res)=>{
-    const city = req.query.city;
+    const startTime = Date.now();
+
+    const query = req.query.city;
     const units = req.query.units
-    if (!city) return res.status(400).send("City is required!");
+    const ip =req.ip;
+    const agent=req.headers["user-agent"];
+    if (!query) return res.status(400).send("City is required!");
     try{
     const result = await axios.get(apiURL+"/forecast",{
         params:{
-            q:city,
+            q:query,
             units: units,
             appid : yourapikey
         }
     });
+
+    const data=result.data;
+    const duration=Date.now()-startTime;
+
+    //extract API values 
+    const city = data.city.name;
+    const country=data.city.country;
+    const lat=data.city.coord.lat;
+    const lon=data.city.coord.lon;
+
+    //Insert into DB
+    await db.query("INSERT INTO search_history(query,city,country,latitude,longitude,units,ip_address,user_agent,response_time_ms,result_status)VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)",[query,city,country,lat,lon,units,ip,agent,duration,"success"]);
+
     res.render("index.ejs",{content: result.data ,type:"forecast"});
     }catch(error){
+
+    const duration = Date.now()-startTime;
+    //log failed
+    await db.query("INSERT INTO search_history(query,units,ip_address,user_agent,response_time_ms,result_status) VALUES ($1,$2,$3,$4,$5,$6)",[query,units,ip,agent,duration,"failed"]);
+
         res.status(404).send(error.message);
     }
 });
 
 app.get("/air_pollution",async(req,res)=>{
-    const city = req.query.city;
-    const units = req.query.units
-    if (!city) return res.status(400).send("City is required!");
+    const startTime = Date.now();
+
+    const query = req.query.city;
+    const units = req.query.units;
+    const ip=req.ip;
+    const agent=req.headers["user-agent"];
+
+    if (!query) return res.status(400).send("City is required!");
     try{
     const weatherResult = await axios.get(apiURL+"/weather",{
         params:{
-            q:city,
+            q:query,
             units: units,
             appid : yourapikey
         }
     });
 
-    const { lat,lon } = weatherResult.data.coord;
-    const  result= await axios.get(apiURL+"/air_pollution",{
+    const weatherData = weatherResult.data;
+    const lat = weatherData.coord.lat;
+    const lon = weatherData.coord.lon;
+    const city = weatherData.name;
+    const country = weatherData.sys.country;
+
+    const  pollutionResult= await axios.get(apiURL+"/air_pollution",{
         params:{
             lat : lat,
             lon : lon,
             appid : yourapikey
         }
     });
+    const duration=Date.now()-startTime;
+
+    //Insert into DB
+    await db.query("INSERT INTO search_history(query,city,country,latitude,longitude,units, ip_address,user_agent,response_time_ms,result_status)VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)",[query,city,country,lat,lon,units,ip,agent,duration,"success"]);
     
-    res.render("index.ejs",{content: result.data ,type:"air_pollution"});
+    res.render("index.ejs",{content: pollutionResult.data ,type:"air_pollution"});
     }catch(error){
+
+    const duration = Date.now()-startTime;
+
+    //log failed
+    await db.query("INSERT INTO search_history(query,units,ip_address,user_agent,response_time_ms,result_status) VALUES ($1,$2,$3,$4,$5,$6)",[query,units,ip,agent,duration,"failed"]);
+
         res.status(404).send(error.message);
     }
 });
